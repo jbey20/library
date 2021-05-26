98a041da4760442392609d685042a1ee92b9b642
Joshua Beychok <joshbeychok3@gmail.com>
2021-05-26 00:52:54 -0400

    added firebase and started integration

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 let myLibrary = [];
 let bookID = 1;
+let user = '';
+let dbRefObject 
 
-function Book(bookID, title, author, pages, read) {
-  this.bookID = bookID;
+function Book(title, author, pages, read) {
   this.title = title;
   this.author = author;
   this.pages = pages;
@@ -13,8 +14,7 @@ function Book(bookID, title, author, pages, read) {
 }
 
 function addBookToLibrary(title, author, pages, read) {
-  const book = new Book(bookID, title, author, pages, read);
-  bookID++;
+  const book = new Book(title, author, pages, read);
   myLibrary.push(book);
 }
 
@@ -40,19 +40,78 @@ function submitForm() {
 }
 
 function displayLibrary() {
+  console.log(`from display library: ${myLibrary}`);
   const shelf = document.querySelector('#library');
   let newRow = shelf.insertRow(-1);
+  
   for (let i = 0; i < myLibrary.length; i++) {
     const newRow = shelf.insertRow(-1);
-    Object.values(myLibrary[i]).forEach(val => {
-      const newCell = newRow.insertCell(-1)
-      const newText = document.createTextNode(`${val}`);
-      newCell.appendChild(newText);
-    })
+    const titleCell = newRow.insertCell(-1);
+    const title = myLibrary[i].title;
+    const titleText = document.createTextNode(`${title}`);
+    titleCell.appendChild(titleText);
+    
+    const authorCell = newRow.insertCell(-1);
+    const author = myLibrary[i].author;
+    const authorText = document.createTextNode(`${author}`);
+    authorCell.appendChild(authorText);
+    
+    const pagesCell = newRow.insertCell(-1);
+    const pages = myLibrary[i].pages;
+    const pagesText = document.createTextNode(`${pages}`);
+    pagesCell.appendChild(pagesText);
+    
+    const readCell = newRow.insertCell(-1);
+    const read = myLibrary[i].read;
+    const readText = document.createTextNode(`${read}`);
+    readCell.appendChild(readText);
+
+    // Object.values(myLibrary[i]).forEach(val => {
+    //   console.log(val);
+    //   const newCell = newRow.insertCell(-1)
+    //   const newText = document.createTextNode(`${val}`);
+    //   newCell.appendChild(newText);
+    // })
   }
   
 }
 
-addBookToLibrary('The Hobbit', 'JRR Tolkien', 295, false);
-addBookToLibrary('Deep Work', 'Cal Newport', 300, true);
-displayLibrary();
+function getUser() {
+  const username = document.querySelector('#username');
+  user = username.value;
+  console.log(user);
+  dbRefObject = firebase.database().ref().child('library').child(`${user}`);
+  dbRefObject.get().then((snapshot) => {
+    if (snapshot.exists()) {
+      myLibrary = [];
+      for (let i = 1; i < snapshot.val().length; i++) {
+        console.log(snapshot.val()[i]);
+        myLibrary.push(snapshot.val()[i]);
+      }
+      console.log(myLibrary);
+    } else {
+      console.log("No data available");
+    }
+  }).catch((error) => {
+    console.error(error);
+  });
+  console.log(myLibrary);
+  displayLibrary();
+}
+
+function write() {
+
+}
+
+
+
+//change arrow function to call the displayLibrary stuff, etc.
+//dbRefObject.on('value', snap => console.log(snap.val()));
+// addBookToLibrary('The Hobbit', 'JRR Tolkien', 100, true);
+// console.log(myLibrary);
+
+const btn = document.querySelector('.username');
+btn.addEventListener('click', () => {
+  console.log('got here');
+  getUser();
+});
