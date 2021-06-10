2555c982ec5e3c80004d99bfa7d38d89262edb45
Joshua Beychok <joshbeychok3@gmail.com>
2021-06-10 00:22:23 -0400

    Fixed bug with needing to display books twice, added write functionality to Firebase, added a function to clear form after submitting or closing it

--- script.js
+++ script.js
@@ -8,9 +8,10 @@ function Book(title, author, pages, read) {
   this.author = author;
   this.pages = pages;
   this.read = read;
-  // this.info = () => {
-  //   return `${title} by ${author}, ${pages} pages, ${read ? 'not read yet' : 'read'}`
-  // }
+}
+
+Book.prototype.toggleRead = function () {
+  //add in checkbox listener at bottom of page that calls this to change the .checked property
 }
 
 function addBookToLibrary(title, author, pages, read) {
@@ -24,6 +25,7 @@ function openForm() {
 
 function closeForm() {
   document.getElementById("addBookForm").style.display = "none";
+  clearForm();
 }
 
 function submitForm() {
@@ -37,20 +39,27 @@ function submitForm() {
   console.log(read);
   addBookToLibrary(title, author, pages, read);
   displayLibrary();
+  write();
+  clearForm();
+}
+
+function clearForm() {
+  const form = document.querySelector('#addForm');
+  form.reset();
 }
 
 function displayLibrary() {
-  console.log(`from display library: ${myLibrary}`);
   const shelf = document.querySelector('#library');
-  let newRow = shelf.insertRow(-1);
+  while (shelf.rows.length > 1) shelf.deleteRow(1);
   
   for (let i = 0; i < myLibrary.length; i++) {
     const newRow = shelf.insertRow(-1);
+    
     const titleCell = newRow.insertCell(-1);
     const title = myLibrary[i].title;
     const titleText = document.createTextNode(`${title}`);
     titleCell.appendChild(titleText);
-    
+        
     const authorCell = newRow.insertCell(-1);
     const author = myLibrary[i].author;
     const authorText = document.createTextNode(`${author}`);
@@ -63,15 +72,14 @@ function displayLibrary() {
     
     const readCell = newRow.insertCell(-1);
     const read = myLibrary[i].read;
-    const readText = document.createTextNode(`${read}`);
-    readCell.appendChild(readText);
-
-    // Object.values(myLibrary[i]).forEach(val => {
-    //   console.log(val);
-    //   const newCell = newRow.insertCell(-1)
-    //   const newText = document.createTextNode(`${val}`);
-    //   newCell.appendChild(newText);
-    // })
+    
+    const readCheck = document.createElement('input');
+    readCheck.type = "checkbox";
+    readCheck.name = "read";
+    readCheck.value = `${read}`;
+    readCheck.checked = read;
+    readCheck.id = "id";
+    readCell.appendChild(readCheck);
   }
   
 }
@@ -79,28 +87,27 @@ function displayLibrary() {
 function getUser() {
   const username = document.querySelector('#username');
   user = username.value;
-  console.log(user);
   dbRefObject = firebase.database().ref().child('library').child(`${user}`);
   dbRefObject.get().then((snapshot) => {
     if (snapshot.exists()) {
       myLibrary = [];
-      for (let i = 1; i < snapshot.val().length; i++) {
+      for (let i = 0; i < snapshot.val().length; i++) {
         console.log(snapshot.val()[i]);
         myLibrary.push(snapshot.val()[i]);
       }
-      console.log(myLibrary);
+      displayLibrary();
     } else {
-      console.log("No data available");
+      alert("User not found");
     }
   }).catch((error) => {
     console.error(error);
   });
-  console.log(myLibrary);
-  displayLibrary();
 }
 
 function write() {
-
+  firebase.database().ref('library/' + user).set(
+    myLibrary
+  )
 }
 
 
@@ -112,6 +119,5 @@ function write() {
 
 const btn = document.querySelector('.username');
 btn.addEventListener('click', () => {
-  console.log('got here');
   getUser();
 });
